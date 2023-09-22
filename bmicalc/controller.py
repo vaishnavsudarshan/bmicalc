705096f45b97804ef96acd2820912e0d5fcbba04
Vaishnav <134876525+vaishnavsudarshan@users.noreply.github.com>
2023-09-22 04:52:06 +0000

    Update to break down height, weight and age

--- bmicalc/controller.py
+++ bmicalc/controller.py
@@ -10,9 +10,9 @@ def update_bmi(_):
     Update BMI and BMI percentile when weight changes
     '''
     gender = document['gender'].value
-    age = float(document['age'].value)
-    weight = float(document['weight'].value)
-    height = float(document['height'].value)
+    age = float(document['age-years'].value)*12 + float(document['age-months'].value)
+    weight = float(document['weight-lb'].value) + float(document['weight-oz'].value)/16
+    height = float(document['height-ft'].value)*12 + float(document['height-in'].value)
     bmi = bmicalc.bmicalc.calc_bmi(weight, height)
     bmi_percentile = bmicalc.bmicalc.calc_percentile(bmi, gender, age)
     document['bmi'].value = f'{bmi:0.2f}'
@@ -24,12 +24,15 @@ def update_weight_from_bmi(_):
     Update weight when BMI changes
     '''
     gender = document['gender'].value
-    age = float(document['age'].value)
-    height = float(document['height'].value)
+    age = float(document['age-years'].value)*12 + float(document['age-months'].value)
+    height = float(document['height-ft'].value)*12 + float(document['height-in'].value)
     bmi = float(document['bmi'].value)
     weight = bmicalc.bmicalc.calc_weight(bmi, height)
     bmi_percentile = bmicalc.bmicalc.calc_percentile(bmi, gender, age)
-    document['weight'].value = f'{weight:0.2f}'
+    weight_lb = int(weight)
+    weight_oz = 16*(weight - weight_lb)
+    document['weight-lb'].value = f'{weight_lb}'
+    document['weight-oz'].value = f'{weight_oz:0.2f}'
     document['bmi-percentile'].value = f'{bmi_percentile:0.2f}'
 
 
@@ -38,11 +41,15 @@ def update_weight_from_bmi_pct(_):
     Update weight when BMI changes
     '''
     gender = document['gender'].value
-    age = float(document['age'].value)
-    height = float(document['height'].value)
+    age = float(document['age-years'].value)*12 + float(document['age-months'].value)
+    height = float(document['height-ft'].value)*12 + float(document['height-in'].value)
     bmi_percentile = float(document['bmi-percentile'].value)
     bmi = bmicalc.bmicalc.calc_bmi_from_percentile(gender, age, bmi_percentile)
     weight = bmicalc.bmicalc.calc_weight(bmi, height)
+    weight_lb = int(weight)
+    weight_oz = 16*(weight - weight_lb)
 
-    document['weight'].value = f'{weight:0.2f}'
+    document['weight-lb'].value = f'{weight_lb}'
+    document['weight-oz'].value = f'{weight_oz:0.2f}'
+
     document['bmi'].value = f'{bmi:0.2f}'
